ae6a418fded5ffd188c8824543f939b51a36af55
Ivy <ivy.trewartha-bustamante@halfords.co.uk>
2026-08-14 13:00:56 +0100

    added new component named Footer

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,17 @@
 import '../css/style.css';
 
 import { Header } from './components/Header.js';
+import { Footer } from './components/Footer.js';
 import { initFlowbite } from 'flowbite';
 
 const header = document.querySelector('#header');
+const footer = document.querySelector('#footer');
 
 if (header) {
     header.innerHTML = Header();
+}
+if (footer) {
+    footer.innerHTML = Footer();
 }
 
 initFlowbite();
